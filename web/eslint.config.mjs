791cd9d8cfbc4898d92397a6fc8c68797ed0c595
MunSeongyun <borygashill608@gmail.com>
2025-03-31 12:17:29 +0900

    first commit

---
+++ web/eslint.config.mjs
@@ -0,0 +1,26 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      quotes: ["error", "double"], // 작은따옴표 강제
+      semi: ["error", "always"],
+      indent: ["error", 2],
+      "comma-dangle": ["error", "always-multiline"],
+      "object-curly-spacing": ["error", "always"],
+      "max-len": ["error", { code: 350 }],
+    },
+  },
+];
+
+export default eslintConfig;
