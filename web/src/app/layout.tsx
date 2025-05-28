511aaa4ce785477dee4309360ccfc025b5b1d4cd
nameless <rrg1591rrg@naver.com>
2025-05-28 09:48:10 +0900

    ✨ feat : html전체에 다크모드 수정

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark:bg-white dark:text-black">
       <body>
         {children}
       </body>
