62ad06d63fbd6f69356b8c3f64a22dfe88759fef
MunSeongyun <borygashill608@gmail.com>
2025-06-17 16:42:16 +0900

    head 추가

---
+++ web/src/app/head.tsx
@@ -0,0 +1,8 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="UBZqAVSxRMQx-RNTswQfq-ltiQ-8EjrzkNLyld0_SP8" />
+    </>
+  );
+}
